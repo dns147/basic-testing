2f41cffe12c69c702a8df320091f2264ee7ff275
dns147 <rarden@ya.ru>
2024-02-11 22:54:46 +0300

    feat: add snapshot-testingt

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,18 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const likedList = generateLinkedList([1, 2, 3, 4, 5]);
+    
+    expect(likedList).toStrictEqual({"next": {"next": {"next": {"next": {"next": {"next": null, "value": null}, "value": 5}, "value": 4}, "value": 3}, "value": 2}, "value": 1});
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const likedList = generateLinkedList([1, 2, 3, 4, 5]);
+
+    expect(likedList).toMatchSnapshot();
   });
 });
